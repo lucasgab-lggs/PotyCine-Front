ab3564c38201402a65ca2e2ec5fbd88abb762be3
mariamanda <amanda.morais42@hotmail.com>
2025-01-15 22:08:56 -0300

    adiciona tela de login

--- potycine-front/src/app/login/page.js
+++ potycine-front/src/app/login/page.js
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import "./loginpage.css"
+
 export default function LoginPage() {
     return (
       <div>
-        Login
+        <div className="my-5">
+          <h1 className="fw-bold">Login</h1>
+          <p className="fs-4">Bem vindo de volta!</p>
+        </div>
+        <form className="login-form">
+          <div className="row row-cols-1 px-4 gap-4">
+            <div className="col">
+              <input
+                class="form-control px-2 py-3"
+                type = 'text'
+                name = 'email'
+                placeholder="Email"
+                required
+              ></input>
+            </div>
+            <div className="col">
+              <input
+                class="form-control px-2 py-3"
+                type = 'password'
+                name = 'password'
+                placeholder="Senha"
+                required
+              ></input>
+            </div>
+            <div className="col d-flex justify-content-end">
+              <a className="fw-bold text-decoration-none esqueceu-senha">Esqueceu a senha?</a>
+            </div>
+            <div className="col">
+              <button className="btn btn-entrar fw-semibold py-2 mt-4" >Entrar</button>
+            </div>
+            <div className="registro-text fs-5">
+              Ainda não possui conta? <Link className="esqueceu-senha fw-bold text-decoration-none" href="/registro">Registre-se</Link>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
